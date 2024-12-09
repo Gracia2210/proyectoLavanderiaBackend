4de585b5bd012bf14a66bb559ef0998f5d5a4888
Gracia Marca <marcaurteagam@gmail.com>
2024-12-09 14:31:50 -0500

    fix: Se agrega prueba para no auth

--- src/main/java/pe/edu/usmp/lavanderia/app/security/ResourceServerConfig.java
+++ src/main/java/pe/edu/usmp/lavanderia/app/security/ResourceServerConfig.java
@@ -21,7 +21,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().
-		antMatchers("/oauth/**","/_ah/start", "/_ah/health").permitAll()
+		antMatchers("/oauth/**","/_ah/start", "/_ah/health", "/cliente/**").permitAll()
 		.anyRequest().authenticated()
 		.and().cors().configurationSource(corsConfigurationSource());
 	}
